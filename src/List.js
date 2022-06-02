f32a9b5a08612bd44b21bfaa9463eab3912f87fa
pedrohsn <pedrodti2017@gmail.com>
2022-06-02 02:23:47 -0300

    implementação dos botões delete e item finalizado

--- src/List.js
+++ src/List.js
@@ -1,17 +1,21 @@
 import React from "react";
 
+function DoneImg(props){
+  if(props.done){
+    return (<img src="./assets/on.png" alt="done"/>)
+  }else{
+    return (<img src="./assets/off.png" alt="undone"/>)
+  }
+}
+
 function List(props){
-
-
-  return(
-            <ul>
-                {props.items.map(item =>
-                <li key={item.id}>
+  return(   <ul>
+                {props.items.map(item => <li className={item.done ? "done" : ""} key={item.id}>
                   {item.text}
+                  <button onClick={()=> {props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
                   <button onClick={()=> {props.onItemDeleted(item)}}><img src="./assets/bin.png" alt="" /></button>
                 </li>)}
-            </ul>
-  )
+            </ul>)
 }
 
 export default List;
--- src/Todo.js
+++ src/Todo.js
@@ -16,15 +16,25 @@ function Todo(){
   }
 
   function onItemDeleted(item){
-    let filteredItems = items.filter(it => it.id != item.id)
+    let filteredItems = items.filter(it => it.id !== item.id)
 
     setItems(filteredItems)
   }
+
+  function onDone(item){
+    let updateItems = items.map(it =>{
+      if(it.id === item.id){
+        it.done = !it.done;
+      }
+      return it;
+    })
+    setItems(updateItems);
+  }
   
   return(<div className="container">
-            <h1>Todo</h1>
+            <h1>To-do</h1>
             <TodoForm onAddItem={onAddItem}></TodoForm>
-            <List onItemDeleted={onItemDeleted}items={items}></List>
+            <List onDone={onDone} onItemDeleted={onItemDeleted}items={items}></List>
          </div>)
 }
 
